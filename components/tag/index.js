c83867fb7540e6457f495f6d52cb9b63dd447f7b
superMin--- <294953530@qq.com>
2022-12-06 13:21:56 +0800

    加入各种功能

---
+++ components/tag/index.js
@@ -0,0 +1,14 @@
+Component({
+    properties: {
+        info: {
+            value: '',
+            type: String
+        },
+        level: {
+            value: 1,
+            type: Number
+        }
+    },
+    data: {},
+    methods: {}
+});
