7c3cb9d6e44d4f98ac926d10c1d2b69272b7f2f5
superMin--- <294953530@qq.com>
2022-12-17 15:07:21 +0800

    rank页面完善

--- pages/rank/index.js
+++ pages/rank/index.js
@@ -1,6 +1,18 @@
-Page({
-    data: {},
-    onLoad: function (options) {
+import {SESSION} from "../../utils/config";
 
-    }
+Page({
+    data: {
+        session: "s7",
+        data: {}
+    },
+    onLoad: function (options) {
+    },
+    onShow: function () {
+        let session = wx.getStorageSync(SESSION),
+            that = this
+
+        that.setData({
+            session: session
+        })
+    },
 });
